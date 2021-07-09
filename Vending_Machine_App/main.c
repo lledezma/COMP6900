e8ed85433855f1ca8f790ac4924ef9004a5325f4
lledezma <luis.ledezma@pop.belmont.edu>
2021-07-08 20:08:50 -0500

    minor update

--- Vending_Machine_App/main.c
+++ Vending_Machine_App/main.c
@@ -155,13 +155,15 @@ int main(void)
 	}
 }
 
-void printString (char * string){
+void printString (char * string)
+{
 	while (*string) // until the string is NULL
 	{
 		UART_OutChar(*(string++));
 		// Read characters from sequentially memory addresses one by one and push them out to PC via UART
 	}
 }
+
 unsigned char UART_InChar(void) 
 {
 	while((UART0_FR_R&0x0010) != 0){
@@ -172,87 +174,95 @@ unsigned char UART_InChar(void)
 		}
 	}
 	return((unsigned char)(UART0_DR_R&0xFF));
-}// Wait for selectionfer to be not full, then output
+}
+
+// Wait for selectionfer to be not full, then output
 void UART_OutChar(unsigned char data) 
 {
 	while((UART0_FR_R&0x0020) != 0);
 	UART0_DR_R = data;
 }
+
 // Assumes a 50 MHz bus clock, creates 115200 baud rate
 void UART_Init(void)
 { // should be called only once
-		SYSCTL_RCGCUART_R |= 0x0001; // activate UART0
-		SYSCTL_RCGCGPIO_R |= 0x0001; // activate port A
-		UART0_CTL_R &= ~0x0001; // disable UART
-		UART0_IBRD_R = 104; // IBRD=int(50000000/(16*115,200)) = int(27.1267)
-		UART0_FBRD_R = 11; // FBRD = round(0.1267 * 64) = 8
-		UART0_LCRH_R = 0x0070; // 8-bit word length, enable FIFO
-		UART0_CTL_R = 0x0301; // enable RXE, TXE and UART
-		GPIO_PORTA_PCTL_R =(GPIO_PORTA_PCTL_R&0xFFFFFF00)+0x00000011; // UART
-		GPIO_PORTA_AMSEL_R &= ~0x03; // disable analog function on PA1-0
-		GPIO_PORTA_AFSEL_R |= 0x03; // enable alt funct on PA1-0
-		GPIO_PORTA_DEN_R |= 0x03; // enable digital I/O on PA1-0
+	SYSCTL_RCGCUART_R |= 0x0001; // activate UART0
+	SYSCTL_RCGCGPIO_R |= 0x0001; // activate port A
+	UART0_CTL_R &= ~0x0001; // disable UART
+	UART0_IBRD_R = 104; // IBRD=int(50000000/(16*115,200)) = int(27.1267)
+	UART0_FBRD_R = 11; // FBRD = round(0.1267 * 64) = 8
+	UART0_LCRH_R = 0x0070; // 8-bit word length, enable FIFO
+	UART0_CTL_R = 0x0301; // enable RXE, TXE and UART
+	GPIO_PORTA_PCTL_R =(GPIO_PORTA_PCTL_R&0xFFFFFF00)+0x00000011; // UART
+	GPIO_PORTA_AMSEL_R &= ~0x03; // disable analog function on PA1-0
+	GPIO_PORTA_AFSEL_R |= 0x03; // enable alt funct on PA1-0
+	GPIO_PORTA_DEN_R |= 0x03; // enable digital I/O on PA1-0
 
 }
-void keypad_init(void){
-			 SYSCTL_RCGCGPIO_R |= 0x04; // activate clock for port C
-			 SYSCTL_RCGCGPIO_R |= 0x08; // activate clock for port E
-	  //ROWS
-			 GPIO_PORTD_DIR_R |= 0x0F; //set row pins 3-0 as output 
-			 GPIO_PORTD_DEN_R |= 0x0F; //set row pins 3-0 as digital pins 
-			 GPIO_PORTD_ODR_R |= 0x0F; //set row pins 3-0 as open drain 
+
+void keypad_init(void)
+{
+	 SYSCTL_RCGCGPIO_R |= 0x04; // activate clock for port C
+	 SYSCTL_RCGCGPIO_R |= 0x08; // activate clock for port E
+	//ROWS
+	 GPIO_PORTD_DIR_R |= 0x0F; //set row pins 3-0 as output 
+	 GPIO_PORTD_DEN_R |= 0x0F; //set row pins 3-0 as digital pins 
+	 GPIO_PORTD_ODR_R |= 0x0F; //set row pins 3-0 as open drain 
 	//COLUMS
-			 GPIO_PORTC_DIR_R &= ~0xF0; //set row pins 7-4as input
-			 GPIO_PORTC_DEN_R |= 0xF0;  
-			 GPIO_PORTC_PUR_R |= 0xF0; 
+	 GPIO_PORTC_DIR_R &= ~0xF0; //set row pins 7-4 as input
+	 GPIO_PORTC_DEN_R |= 0xF0;  
+	 GPIO_PORTC_PUR_R |= 0xF0; 
 }
+
 unsigned char keypad_getkey(void)
 {
-		const unsigned char keymap[4][4] = {
-				{ '1', '2', '3', 'A'},
-				{ '4', '5', '6', 'B'},
-				{ '7', '8', '9', 'C'},
-				{ 'Y', '0', 'N', 'D'},
-		};
+	const unsigned char keymap[4][4] = 
+	{
+		{ '1', '2', '3', 'A'},
+		{ '4', '5', '6', 'B'},
+		{ '7', '8', '9', 'C'},
+		{ 'Y', '0', 'N', 'D'},
+	};
  
-int row, col;
-// check to see any key pressed 
-GPIO_PORTD_DATA_R = 0;
-col = GPIO_PORTC_DATA_R & 0xF0;
-if (col == 0xF0) 
-	return 0; //no press
- 
-while (1)
-{
-	row = 0;
-	GPIO_PORTD_DATA_R = 0x0E;
-	delayUs(2); 
+	int row, col;
+	// check to see any key pressed 
+	GPIO_PORTD_DATA_R = 0;
 	col = GPIO_PORTC_DATA_R & 0xF0;
-	if (col != 0xF0) break;
-	row = 1;
-	GPIO_PORTD_DATA_R = 0x0D;
-	delayUs(2); 
-	col = GPIO_PORTC_DATA_R & 0xF0;
-	if (col != 0xF0) break;
-	row = 2;
-	GPIO_PORTD_DATA_R = 0x0B;
-	delayUs(2); 
-	col = GPIO_PORTC_DATA_R & 0xF0;
-	if (col != 0xF0) break;
-	row = 3;
-	GPIO_PORTD_DATA_R = 0x07;
-	delayUs(2); 
-	col = GPIO_PORTC_DATA_R & 0xF0;
-	if (col != 0xF0) 
-		break;
-	return 0; // no key was pressed
+	if (col == 0xF0) 
+		return 0; //no press
+	 
+	while (1)
+	{
+		row = 0;
+		GPIO_PORTD_DATA_R = 0x0E;
+		delayUs(2); 
+		col = GPIO_PORTC_DATA_R & 0xF0;
+		if (col != 0xF0) break;
+		row = 1;
+		GPIO_PORTD_DATA_R = 0x0D;
+		delayUs(2); 
+		col = GPIO_PORTC_DATA_R & 0xF0;
+		if (col != 0xF0) break;
+		row = 2;
+		GPIO_PORTD_DATA_R = 0x0B;
+		delayUs(2); 
+		col = GPIO_PORTC_DATA_R & 0xF0;
+		if (col != 0xF0) break;
+		row = 3;
+		GPIO_PORTD_DATA_R = 0x07;
+		delayUs(2); 
+		col = GPIO_PORTC_DATA_R & 0xF0;
+		if (col != 0xF0) 
+			break;
+		return 0; // no key was pressed
+	}
+	if (col == 0xE0) return keymap[row][0]; // key in column 0 
+	if (col == 0xD0) return keymap[row][1]; // key in column 1 
+	if (col == 0xB0) return keymap[row][2]; // key in column 2 
+	if (col == 0x70) return keymap[row][3]; // key in column 3 
+	return 0; 
 }
-if (col == 0xE0) return keymap[row][0]; // key in column 0 
-if (col == 0xD0) return keymap[row][1]; // key in column 1 
-if (col == 0xB0) return keymap[row][2]; // key in column 2 
-if (col == 0x70) return keymap[row][3]; // key in column 3 
-return 0; 
-}
+
 /* delay n microseconds */
 void delayUs(int n)
 {
